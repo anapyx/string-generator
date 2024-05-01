71e9132167bf4cab76e63b27f19a04d1d0954346
Ana <ana.cabral@academico.ufpb.br>
2024-05-01 17:24:30 -0300

    Fast Mode UPDATE

--- main.py
+++ main.py
@@ -59,52 +59,63 @@ for prod in prod_list:
 
 print('gerou producoes')
 print(productions)
+
 # FAST MODE
 
 def fast_mode():
     chain = ""
     chain_path = []
     chain_path.append("epsilon")
-    epsilon_possibility = None  
-    for x in productions:
-      for y in productions[x]:
-        if y == "epsilon":
-          chain_path.append(x)
-          break
-      if len(chain_path) != 0:
-        break   
-    i = 1
-    while chain_path[i - 1] != initial_var:
-      for x in productions:
-        for y in productions[x]:
-          if chain_path[i - 1] in y:
-            chain_path.append(x)
-            i += 1
-            break
-        if chain_path[i - 1] == initial_var:
-          break 
-    print("Derivacao:")
-    chain_sub_str = []
-    chain_sub_str.append(str(chain_path[len(chain_path) - 1]) + " -> ")
-    chain += chain_sub_str[0]
-    j = 0
-    for i in range(len(chain_path)-1, 0, -1):
-      for x in productions[chain_path[i]]:
-        if chain_path[i - 1] in x:
-          match = re.search(r'(.*?)(?= ->)', chain_sub_str[j])
-          if match:
-            if "epsilon" in x:
-              x = ""
-              aux = match.group(0).replace(chain_path[i], x, 1)
-              chain_sub_str.append(aux)
-            else:
-              aux = match.group(0).replace(chain_path[i], x, 1)
-              chain_sub_str.append(aux + " -> ")
-            j += 1
-            chain += chain_sub_str[j]   
-    print(chain)    
-    print("Cadeia gerada:")
-    print(chain_sub_str[len(chain_sub_str) - 1])    
+    epsilon_possibility = None
+
+    continue_fast_mode = True
+    while True:
+        for x in productions:
+          for y in productions[x]:
+            if y == "epsilon":
+              chain_path.append(x)
+              break
+          if len(chain_path) != 0:
+            break   
+        i = 1
+
+        while chain_path[i - 1] != initial_var:
+          for x in productions:
+            for y in productions[x]:
+              if chain_path[i - 1] in y:
+                chain_path.append(x)
+                i += 1
+                break
+            if chain_path[i - 1] == initial_var:
+              break 
+
+        print("Derivacao:")
+        chain_sub_str = []
+        chain_sub_str.append(str(chain_path[len(chain_path) - 1]) + " -> ")
+        chain += chain_sub_str[0]
+        j = 0
+        for i in range(len(chain_path)-1, 0, -1):
+          for x in productions[chain_path[i]]:
+            if chain_path[i - 1] in x:
+              match = re.search(r'(.*?)(?= ->)', chain_sub_str[j])
+              if match:
+                if "epsilon" in x:
+                  x = ""
+                  aux = match.group(0).replace(chain_path[i], x, 1)
+                  chain_sub_str.append(aux)
+                else:
+                  aux = match.group(0).replace(chain_path[i], x, 1)
+                  chain_sub_str.append(aux + " -> ")
+                j += 1
+                chain += chain_sub_str[j]   
+
+        print(chain)    
+        print("Cadeia gerada:")
+        print(chain_sub_str[len(chain_sub_str) - 1])    
+        print("\nDeseja gerar outra cadeia? (s/n)")
+        keep = input()
+        if keep.lower() != 's':
+            continue_fast_mode = False
 
 
 # DETAILED MODE    
