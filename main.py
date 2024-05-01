b360f743fe51a026e677c78c396449cd579b303d
Ana <ana.cabral@academico.ufpb.br>
2024-05-01 18:38:35 -0300

    Bug fixes

--- main.py
+++ main.py
@@ -60,7 +60,34 @@ for prod in prod_list:
 print('gerou producoes')
 print(productions)
 
+def validEntries():
+    flags = []
+    output = True
+
+    if len(variables) < 1:
+       flags.append(1)
+       output = False
+
+    if (not initial_var.isalpha()) and (not(initial_var in variables)) and (not len(initial_var) == 1):
+       flags.append(2)
+       output = False
+
+    if len(terminals) < 1:
+       flags.append(3)
+
+    for key in productions.keys():
+       if key not in variables:
+          flags.append(4)
+          output = False
+
+    return output, flags
+
+validation, validation_flags = validEntries()
+print(validation)
+print(validation_flags)
+
 # FAST MODE
+
 
 def fast_mode():
     chain = ""
@@ -71,23 +98,23 @@ def fast_mode():
     continue_fast_mode = True
     while continue_fast_mode:
         for x in productions:
-          for y in productions[x]:
-            if y == "epsilon":
-              chain_path.append(x)
-              break
-          if len(chain_path) != 0:
-            break   
+            for y in productions[x]:
+                if y == "epsilon":
+                    chain_path.append(x)
+                    break
+            if len(chain_path) != 0:
+                break   
         i = 1
 
         while chain_path[i - 1] != initial_var:
-          for x in productions:
-            for y in productions[x]:
-              if chain_path[i - 1] in y:
-                chain_path.append(x)
-                i += 1
-                break
-            if chain_path[i - 1] == initial_var:
-              break 
+            for x in productions:
+                for y in productions[x]:
+                    if chain_path[i - 1] in y:
+                        chain_path.append(x)
+                        i += 1
+                        break
+                if chain_path[i - 1] == initial_var:
+                    break 
 
         print("Derivacao:")
         chain_sub_str = []
@@ -95,23 +122,24 @@ def fast_mode():
         chain += chain_sub_str[0]
         j = 0
         for i in range(len(chain_path)-1, 0, -1):
-          for x in productions[chain_path[i]]:
-            if chain_path[i - 1] in x:
-              match = re.search(r'(.*?)(?= ->)', chain_sub_str[j])
-              if match:
-                if "epsilon" in x:
-                  x = ""
-                  aux = match.group(0).replace(chain_path[i], x, 1)
-                  chain_sub_str.append(aux)
-                else:
-                  aux = match.group(0).replace(chain_path[i], x, 1)
-                  chain_sub_str.append(aux + " -> ")
-                j += 1
-                chain += chain_sub_str[j]   
+            for x in productions[chain_path[i]]:
+                if chain_path[i - 1] in x:
+                    match = re.search(r'(.*?)(?= ->)', chain_sub_str[j])
+                    if match:
+                        if "epsilon" in x:
+                            x = ""
+                            aux = match.group(0).replace(chain_path[i], x, 1)
+                            chain_sub_str.append(aux)
+                        else:
+                            aux = match.group(0).replace(chain_path[i], x, 1)
+                            chain_sub_str.append(aux + " -> ")
+                        j += 1
+                        chain += chain_sub_str[j]   
 
         print(chain)    
         print("Cadeia gerada:")
-        print(chain_sub_str[len(chain_sub_str) - 1])    
+        print(chain_sub_str[len(chain_sub_str) - 1])  
+
         print("\nDeseja gerar outra cadeia? (s/n)")
         keep = input()
         if keep.lower() != 's':
@@ -132,28 +160,28 @@ def detailed_mode():
         operation = input()
         operacao_valida = False
         for x in productions[current_variable]:
-          if operation in x:
-            operacao_valida = True
-            break
-        if operacao_valida:
-          match = re.search(r'(.*?)(?= ->)', chain_sub_str)
-          if match:
-            if "epsilon" in operation:
-              aux = ""
-              aux = match.group(0).replace(current_variable, aux, 1)
-              chain_sub_str = aux
-            else:
-              aux = match.group(0).replace(current_variable, operation, 1)
-              chain_sub_str = aux + " -> "
-            chain += chain_sub_str
-            print(chain)
-          if operation == "epsilon":
-            current_variable = operation
-          else:
-            for x in variables:
-              if x in operation:
-                current_variable = x
+            if operation in x:
+                operacao_valida = True
                 break
+        if operacao_valida:
+            match = re.search(r'(.*?)(?= ->)', chain_sub_str)
+            if match:
+                if "epsilon" in operation:
+                    aux = ""
+                    aux = match.group(0).replace(current_variable, aux, 1)
+                    chain_sub_str = aux
+                else:
+                    aux = match.group(0).replace(current_variable, operation, 1)
+                    chain_sub_str = aux + " -> "
+                chain += chain_sub_str
+                print(chain)
+            if operation == "epsilon":
+                current_variable = operation
+            else:
+                for x in variables:
+                    if x in operation:
+                        current_variable = x
+                        break
         else:
             print("Operacao invalida!") 
 
